81261188c501a34eebf9f29f0be24fe4cd8e1017
513667 <thewantedgunnl@gmail.com>
2015-04-15 13:16:37 +0000

    v0.2

--- index.php
+++ index.php
@@ -10,9 +10,11 @@
         <!-- Javascript -->
         <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
         <script type="text/javascript" src="js/java.css"></script>
+        
     </head>
     
     <body>
+        <body class="duty-bg">
         <div class="body">
             <div class="menubalk">
                 <h1>Homepage</h1>
