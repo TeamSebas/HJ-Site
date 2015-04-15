cd9ad4182cb8062aa0e0fd3eef017433e75d3a9c
Sebastiaan Dirks <513811@lln.corlaercollege.nl>
2015-04-15 08:29:53 +0000

    Go-HJ!

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Homepage - HJ</title>
+        <meta charset="utf-8">
+        
+        <!-- Style.css -->
+        <link rel="stylesheet" href="css/style.css" type="text/css" />
+        
+        <!-- Javascript -->
+        <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+        <script type="text/javascript" src="js/java.css"></script>
+    </head>
+    
+    <body>
+        
+    </body>
+</html>
